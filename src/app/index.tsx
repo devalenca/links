435924bd244b3b882bc99535a13d3d34f73200a2
Gabriel Alencar <117116076+devalenca@users.noreply.github.com>
2026-03-02 16:53:11 -0300

    Add splash screen image and initial app index component
    - Added a new splash.png image to the assets/images directory.
    - Created an index.tsx file in the src/app directory with a basic React Native component displaying welcome messages.

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Welcome to Links!</Text>
+      <Text>Open up src/app/index.tsx to start working on your app!</Text>
+    </View>
+  );
+}
