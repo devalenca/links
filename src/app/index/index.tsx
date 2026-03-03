2c3fff23f3f31c0cffae44bf47d601b235307b53
devalenca <alencargabriel1207@gmail.com>
2026-03-03 20:55:09 -0300

    Add Category component and categories utility

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,6 @@
+import { Category } from "@/components/category";
 import { colors } from "@/styles/colors";
+import { categories } from "@/utils/categories";
 import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 import { Image, TouchableOpacity, View } from "react-native";
@@ -14,6 +16,36 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+      <Category
+        name={categories[0].name}
+        icon={categories[0].icon}
+        isSelected
+      />
+      <Category
+        name={categories[1].name}
+        icon={categories[1].icon}
+        isSelected={false}
+      />
+      <Category
+        name={categories[2].name}
+        icon={categories[2].icon}
+        isSelected={false}
+      />
+      <Category
+        name={categories[3].name}
+        icon={categories[3].icon}
+        isSelected={false}
+      />
+      <Category
+        name={categories[4].name}
+        icon={categories[4].icon}
+        isSelected={false}
+      />
+      <Category
+        name={categories[5].name}
+        icon={categories[5].icon}
+        isSelected={false}
+      />
     </View>
   );
 }
